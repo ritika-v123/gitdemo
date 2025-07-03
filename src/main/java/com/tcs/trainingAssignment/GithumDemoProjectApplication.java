5011e391ee40b35252730de9e86c539ce90f07ac
Ritika <ritikv013@gmail.com>
2025-07-03 07:27:59 +0000

    first commit

---
+++ src/main/java/com/tcs/trainingAssignment/GithumDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.trainingAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithumDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithumDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs/trainingAssignment/GithumDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.trainingAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithumDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
